5594b9aab0eae54f796e080371e38c324f361e8d
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-12 01:33:46 -0400

    Primeiro Commit

---
+++ src/HoraSistema.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class HoraSistema {
+  public static void main(String[] args) {
+    Date relogio = new Date();
+    System.out.println("A hora do Sistema é: " + relogio);
+  }
+}
