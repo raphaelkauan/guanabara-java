1d9e83bf6cc91aad7cd2c09dedc118f2b460f9d4
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-13 14:52:49 -0400

    Adicionando Sistema de Notas

---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TiposPrimitivos {
+
+  public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    System.out.println("Digite o nome do Aluno: ");
+    String nome = teclado.nextLine();
+    System.out.println("Digite a nota do Aluno: ");
+    float nota = teclado.nextFloat();
+    
+    System.out.println("O nome do aluno é " + nome + " E sua nota é " + nota);
+  }
+}
