935d994594b41bc9f77673e913e7808c2b5f043e
Raphael Kauan <raphaelkauanoficial@gmail.com>
2023-03-25 22:08:06 -0400

    novos projetinhos simples

---
+++ src/curso/java/segundasemana/OperadoresLogicos.java
@@ -0,0 +1,19 @@
+package curso.java.segundasemana;
+
+public class OperadoresLogicos {
+	
+	public static void main(String[] args) {
+		
+		int x = 4;
+		int y = 7;
+		int z = 12;
+		
+		if (x < y || y == z) {
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+		
+	}
+	
+}
